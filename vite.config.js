9ab364855c04718c6beb0c6edd458f8a433e5014
MrCooksIT <acoetzee@maristsj.co.za>
2024-11-22 10:01:03 +0200

    Fix date-fns dependency

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['date-fns'],
+      output: {
+        globals: {
+          'date-fns': 'dateFns'
+        }
+      }
+    }
   }
 })
